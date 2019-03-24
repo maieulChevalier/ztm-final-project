cb9eadfa6471bab59cd09ee168ce5cc7deede1fb
maieulChevalier <maieul.chevalier@gmail.com>
2019-03-24 16:17:51 +0800

    fixing keroku error with port 3000 by changing it to process.env.PORT

--- server.js
+++ server.js
@@ -123,8 +123,8 @@ app.put('/image', (req, res) => {
     .catch(err => res.status(400).json('unable to get entries'))
 })
 
-app.listen(3001, () => {
-    console.log('app is running on port 3001');
+app.listen(process.env.PORT || 3001, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
 
 /* 
